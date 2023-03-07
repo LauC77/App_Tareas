1401cfeb4e8d71336acecf239376c3fe451f7c6f
Laura <valencastro943@gmail.com>
2023-03-07 13:53:26 -0500

    Clase 03 completa

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const btn= document.querySelector("[data-form-btn]")
 const createTask =(evento) => {
     evento.preventDefault();
     const input= document.querySelector("[data-form-input]");
+    const value =input.value;
+    const list=document.querySelector("[data-list]");
+    input.value="";
+    const task= document.createElement("li")
+    task.classList.add("card")
+    const content= `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML= content;
+
+  list.appendChild(task);
+
+    console.log(value);
 }
 
 btn.addEventListener("click", createTask);
